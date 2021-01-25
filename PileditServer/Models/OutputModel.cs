ace617f1c027f1fb0fca9f4f59d55869027c7535
kuro1215 <s.kuro1215@gmail.com>
2021-01-25 18:18:36 +0900

    🌱出力リクエストクラスを作成

---
+++ PileditServer/Models/OutputModel.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PileditServer.Models
+{
+    public class OutputRequest
+    {
+        public string Uuid { get; set; }
+        public string Extention { get; set; }
+        public string FourCC { get; set; }
+    }
+}
