a82e201990840dd3d71fb4b74a016577f7724457
kuro1215 <s.kuro1215@gmail.com>
2021-01-25 18:12:40 +0900

    ⛓クラス名変更に伴う修正

--- PileditBackend/IO/FileName.cs
+++ PileditBackend/IO/FileName.cs
@@ -11,22 +11,22 @@ namespace PileditBackend.IO
             if (!IsExsitType(type)) return (null, null);
             string s = result ? "_result" : "";
             var date = DateTime.Now;
-            string file = $@"{MESystem.JsonWatchPath}\{type}\{type}{date:HHmmssf}{s}.{extention}";
+            string file = $@"{PileditSystem.JsonWatchPath}\{type}\{type}{date:HHmmssf}{s}.{extention}";
             return (date, file);
         }
         public static string GetDate(string path)
         {
-            if (path == null || !path.Contains(MESystem.JsonWatchPath, CurrentCulture)) return null;
+            if (path == null || !path.Contains(PileditSystem.JsonWatchPath, CurrentCulture)) return null;
             string name = Path.GetFileNameWithoutExtension(path).Replace("_result", "", CurrentCulture);
             return name[^8..];
         }
         public static string GetType(string path)
         {
-            if (path == null || !path.Contains(MESystem.JsonWatchPath, CurrentCulture)) return null;
-            return Path.GetDirectoryName(path).Replace($@"{MESystem.JsonWatchPath}\", "", CurrentCulture);
+            if (path == null || !path.Contains(PileditSystem.JsonWatchPath, CurrentCulture)) return null;
+            return Path.GetDirectoryName(path).Replace($@"{PileditSystem.JsonWatchPath}\", "", CurrentCulture);
         }
         public static bool IsExsitType(string type)
-            => Directory.Exists($@"{MESystem.JsonWatchPath}\{type}");
+            => Directory.Exists($@"{PileditSystem.JsonWatchPath}\{type}");
         public static bool IsResultFile(string path)
             => Path.GetFileNameWithoutExtension(path).EndsWith("_result", CurrentCulture);
     }
