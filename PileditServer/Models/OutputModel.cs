128705c851af2e547dbf925499166de492981598
kuro1215 <s.kuro1215@gmail.com>
2021-01-26 11:09:17 +0900

    ➖不要なusingの削除

--- PileditServer/Models/OutputModel.cs
+++ PileditServer/Models/OutputModel.cs
@@ -1,11 +1,4 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace PileditServer.Models
+﻿namespace PileditServer.Models
 {
     public class OutputRequest
     {
